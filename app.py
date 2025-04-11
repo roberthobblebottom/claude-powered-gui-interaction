0f1548fae6d4793fa3933e28b0094ce469f5009c
themissingCRAM <pyroclastic@protonmail.com>
2025-04-11 20:04:52 +0800

    key test

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-
 from flask import Flask, render_template, request
 
-
-import os
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    print(os.system("ls"))
+def home_page():
     return render_template("index.html")
-
 
 @app.route("/click_event/", methods=["POST"])
 def click_event():
