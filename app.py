9dbed9de17d0c6610b74ec3d1b8869e7ea738722
themissingCRAM <pyroclastic@protonmail.com>
2025-04-11 21:08:53 +0800

    more automation

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from time import sleep
+
 from flask import Flask, render_template, request
 import webbrowser
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,6 +16,8 @@ def click_event():
     print('button_clicked',button_clicked)
     return render_template('click_event.html',button_clicked = button_clicked)
 if __name__ == "__main__":
+    os.system("pgrep -f 'python3 mouse_demo.py' | xargs kill -9 ")
+    sleep(1)
     webbrowser.open_new("http://127.0.0.1:5000")
     app.run()
 
--- mouse_demo.py
+++ mouse_demo.py
@@ -1,12 +1,13 @@
 import anthropic
 import  os
-
+from time import sleep
 if __name__ == '__main__':
     with open( "anthropic_api_key.txt","r") as f:
         os.environ["ANTHROPIC_API_KEY"]= f.read()
     client = anthropic.Anthropic()
     # print(os.environ['ANTHROPIC_API_KEY'])
     os.system("python3 app.py")
+    sleep(0.5)
     #
     # response = client.beta.messages.create(
     #     model="claude-3-7-sonnet-20250219",
@@ -15,8 +16,8 @@ if __name__ == '__main__':
     #         {
     #           "type": "computer_20250124",
     #           "name": "computer",
-    #           "display_width_px": 1024,
-    #           "display_height_px": 768,
+    #           "display_width_px": 3840,
+    #           "display_height_px": 2160,
     #           "display_number": 1,
     #         },
     #
