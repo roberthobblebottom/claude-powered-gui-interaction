f1d2131b1894d60c12a11d40b4574ae0a4b33a5e
themissingCRAM <pyroclastic@protonmail.com>
2025-04-11 20:35:33 +0800

    mouse demo

--- mouse_demo.py
+++ mouse_demo.py
@@ -1,1 +1,9 @@
 import anthropic
+import  os
+
+if __name__ == '__main__':
+    with open( "anthropic_api_key.txt","r") as f:
+        os.environ["ANTHROPIC_API_KEY"]= f.read()
+    client = anthropic.Anthropic()
+    # print(os.environ['ANTHROPIC_API_KEY'])
+
