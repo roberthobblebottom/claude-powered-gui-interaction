37a70c1759e59671ba6f23216c1d59b632a4191d
themissingCRAM <pyroclastic@protonmail.com>
2025-04-11 21:01:47 +0800

    mouse demo run browser and some anthropic lib init

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import webbrowser
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +11,7 @@ def click_event():
     button_clicked = request.form.get('button')
     print('button_clicked',button_clicked)
     return render_template('click_event.html',button_clicked = button_clicked)
-
 if __name__ == "__main__":
+    webbrowser.open_new("http://127.0.0.1:5000")
     app.run()
 
--- mouse_demo.py
+++ mouse_demo.py
@@ -6,4 +6,23 @@ if __name__ == '__main__':
         os.environ["ANTHROPIC_API_KEY"]= f.read()
     client = anthropic.Anthropic()
     # print(os.environ['ANTHROPIC_API_KEY'])
-
+    os.system("python3 app.py")
+    #
+    # response = client.beta.messages.create(
+    #     model="claude-3-7-sonnet-20250219",
+    #     max_tokens=1024,
+    #     tools=[
+    #         {
+    #           "type": "computer_20250124",
+    #           "name": "computer",
+    #           "display_width_px": 1024,
+    #           "display_height_px": 768,
+    #           "display_number": 1,
+    #         },
+    #
+    #     ],
+    #     messages=[{"role": "user", "content": "Click the Yes Button"}],
+    #     betas=["computer-use-2025-01-24"],
+    #     thinking={"type": "enabled", "budget_tokens": 1024}
+    # )
+    # print(response)
