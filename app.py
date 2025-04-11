debc5e9b946595e72ca051cd3337a30b9f223f6b
themissingCRAM <pyroclastic@protonmail.com>
2025-04-11 19:54:10 +0800

    website

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-import flask
+
+from flask import Flask, render_template, request
+
+
+import os
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print(os.system("ls"))
+    return render_template("index.html")
+
+
+@app.route("/click_event/", methods=["POST"])
+def click_event():
+    button_clicked = request.form.get('button')
+    print('button_clicked',button_clicked)
+    return render_template('click_event.html',button_clicked = button_clicked)
+
+if __name__ == "__main__":
+    app.run()
